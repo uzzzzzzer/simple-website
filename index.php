e4201fd8ed4de9b9bae3418af8a8a3871adbed32
uzzzzzzer <79088180+uzzzzzzer@users.noreply.github.com>
2022-03-07 01:07:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <?php
-header("Location: https://evil-ds.herokuapp.com/star_destroyer/main.html");
+header("Location: https://evil-ds.herokuapp.com/star destroyer/main.html");
 include_once("star destroyer/main.html"); ?> 
 <style>
 <?php include 'star destroyer/css/style-main.css'; ?>
