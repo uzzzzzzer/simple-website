40b57cc444ff077280f9b1b8cf27f7eb51f8f0bb
uzzzzzzer <79088180+uzzzzzzer@users.noreply.github.com>
2022-03-07 00:23:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php include_once("star destroyer/main.html");include_once("star destroyer/"); ?> 
+<?php include_once("star destroyer/main.html");include_once("star destroyer/css/style-main.css"); ?> 
